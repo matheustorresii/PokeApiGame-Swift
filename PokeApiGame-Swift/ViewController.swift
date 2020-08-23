34c6ced8aea15c55a574e742010381e9ab715e6f
matheustorres <matheustf2003@gmail.com>
2020-08-23 01:43:13 -0300

    landscape and functions

--- PokeApiGame-Swift/ViewController.swift
+++ PokeApiGame-Swift/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var player: UIImageView!
+  @IBOutlet weak var adventure: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
-
+  @IBAction func goUp(_ sender: Any) {
+  }
+  
+  @IBAction func goRight(_ sender: Any) {
+  }
+  
+  @IBAction func goDown(_ sender: Any) {
+  }
+  
+  @IBAction func goLeft(_ sender: Any) {
+  }
+  
+  @IBAction func capture(_ sender: Any) {
+  }
 }
 
