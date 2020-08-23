a3cab307edf982431fbc3755f5ef453cd69e0425
matheustorres <matheustf2003@gmail.com>
2020-08-23 14:56:21 -0300

    movement

--- PokeApiGame-Swift/ViewController.swift
+++ PokeApiGame-Swift/ViewController.swift
@@ -13,20 +13,56 @@ class ViewController: UIViewController {
   @IBOutlet weak var player: UIImageView!
   @IBOutlet weak var adventure: UIImageView!
   
+  var playerX: CGFloat = 0
+  var playerY: CGFloat = 0
+  var adventureTile: Float = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    adventureTile = Float(adventure.frame.width)/8.2
+    playerX = player.frame.origin.x
+    playerY = player.frame.origin.y
   }
 
   @IBAction func goUp(_ sender: Any) {
-  }
-  
-  @IBAction func goRight(_ sender: Any) {
+    player.image = UIImage(named: "up")
+    player.frame.origin = CGPoint(
+      x: playerX,
+      y: playerY - CGFloat(adventureTile)
+    )
+    playerX = player.frame.origin.x
+    playerY = player.frame.origin.y
   }
   
   @IBAction func goDown(_ sender: Any) {
+    player.image = UIImage(named: "down")
+    player.frame.origin = CGPoint(
+      x: playerX,
+      y: playerY + CGFloat(adventureTile)
+    )
+    playerX = player.frame.origin.x
+    playerY = player.frame.origin.y
   }
   
   @IBAction func goLeft(_ sender: Any) {
+    player.image = UIImage(named: "left")
+    player.frame.origin = CGPoint(
+      x: playerX - CGFloat(adventureTile),
+      y: playerY
+    )
+    playerX = player.frame.origin.x
+    playerY = player.frame.origin.y
+  }
+  
+  @IBAction func goRight(_ sender: Any) {
+    player.image = UIImage(named: "right")
+    player.frame.origin = CGPoint(
+      x: playerX + CGFloat(adventureTile),
+      y: playerY
+    )
+    playerX = player.frame.origin.x
+    playerY = player.frame.origin.y
   }
   
   @IBAction func capture(_ sender: Any) {
