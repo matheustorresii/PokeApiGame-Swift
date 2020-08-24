d5b01703bb6f7c2bfd6ef3835ac6585b26ab27ea
matheustorres <matheustf2003@gmail.com>
2020-08-24 16:43:13 -0300

    api request, pokemon encounter and pokemon capture

--- PokeApiGame-Swift/ViewController.swift
+++ PokeApiGame-Swift/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var player: UIImageView!
   @IBOutlet weak var adventure: UIImageView!
+  @IBOutlet weak var pokemonImage: UIImageView!
+  @IBOutlet weak var lastPokemonLabel: UILabel!
+  
+  let pokemonsInRoute: [Int] = [1, 4, 7]
   
   var playerX: CGFloat = 0
   var playerY: CGFloat = 0
   var adventureTile: Float = 0
+  
+  var currentPokemon: String = ""
+  
+  var onBattle: Bool = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,6 +33,7 @@ class ViewController: UIViewController {
   }
 
   @IBAction func goUp(_ sender: Any) {
+    if onBattle { return }
     player.image = UIImage(named: "up")
     if playerY < CGFloat(adventureTile * 2) {
       return
@@ -35,9 +44,11 @@ class ViewController: UIViewController {
     )
     playerX = player.frame.origin.x
     playerY = player.frame.origin.y
+    checkGrass()
   }
   
   @IBAction func goDown(_ sender: Any) {
+    if onBattle { return }
     player.image = UIImage(named: "down")
     if playerY > CGFloat(adventureTile * 6) {
       return
@@ -48,9 +59,11 @@ class ViewController: UIViewController {
     )
     playerX = player.frame.origin.x
     playerY = player.frame.origin.y
+    checkGrass()
   }
   
   @IBAction func goLeft(_ sender: Any) {
+    if onBattle { return }
     player.image = UIImage(named: "left")
     if playerX < CGFloat(adventureTile) {
       return
@@ -61,9 +74,11 @@ class ViewController: UIViewController {
     )
     playerX = player.frame.origin.x
     playerY = player.frame.origin.y
+    checkGrass()
   }
   
   @IBAction func goRight(_ sender: Any) {
+    if onBattle { return }
     player.image = UIImage(named: "right")
     if playerX > CGFloat(adventureTile * 6) {
       return
@@ -74,9 +89,77 @@ class ViewController: UIViewController {
     )
     playerX = player.frame.origin.x
     playerY = player.frame.origin.y
+    checkGrass()
   }
   
   @IBAction func capture(_ sender: Any) {
+    if !onBattle { return }
+    onBattle = false
+    adventure.image = UIImage(named: "route")
+    DispatchQueue.main.async {
+      self.player.image = UIImage(named: "down")
+      self.pokemonImage.image = nil
+    }
+    lastPokemonLabel.text = "Last Pokemon: \(currentPokemon)"
+  }
+  
+  func checkGrass(){
+    if playerX < CGFloat(adventureTile * 5), playerY > CGFloat(adventureTile * 6){
+      genPokemon()
+    }else if playerX < CGFloat(adventureTile * 2), playerY > CGFloat(adventureTile * 5){
+      genPokemon()
+    }else if playerX < CGFloat(adventureTile), playerY > CGFloat(adventureTile * 4){
+      genPokemon()
+    }
+  }
+  
+  func genPokemon(){
+    let random = Int.random(in: 0...2)
+    if(random == 0){
+      let randomPokemonId: Int = pokemonsInRoute[Int.random(in: 0..<pokemonsInRoute.count)]
+      fetchPokemon(randomPokemonId)
+    }
+  }
+  
+  func fetchPokemon(_ pokemonId: Int){
+    let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonId)")
+    
+    let task = URLSession.shared.dataTask(with: url!){
+      (data, response, error) in
+      if let data = data {
+        if let json = try? JSONDecoder().decode(Pokemon.self, from: data){
+          self.changeForBattle(json)
+        }
+      }
+    }
+    task.resume()
+  }
+  
+  func changeForBattle(_ encounteredPokemon: Pokemon){
+    onBattle = true
+    downloadImage(urlstr: "https://i.imgur.com/B6uhcTZ.png", imageView: adventure)
+    downloadImage(
+      urlstr: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/00\(encounteredPokemon.id).png",
+      imageView: pokemonImage)
+    currentPokemon = encounteredPokemon.name
+    DispatchQueue.main.async {
+      self.player.image = nil
+    }
+  }
+  
+  func downloadImage(urlstr: String, imageView: UIImageView) {
+      let url = URL(string: urlstr)!
+      let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+          guard let data = data else { return }
+          DispatchQueue.main.async { // Make sure you're on the main thread here
+              imageView.image = UIImage(data: data)
+          }
+      }
+      task.resume()
   }
 }
 
+struct Pokemon: Decodable {
+  var id: Int
+  var name: String
+}
